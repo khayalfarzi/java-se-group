72ed4b2262875bc26600fa982e4a6cfa0843789d
khayalfarzi <xeyalferziyev9@gmail.com>
2021-03-31 20:49:37 +0300

    Initial commit

---
+++ src/main/java/az/none/javalessons/lesson_1/Main.java
@@ -0,0 +1,19 @@
+package az.none.javalessons.lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean a = false, b = false, c = false;
+
+        int first=5;
+        int second=10;
+        int sum = first+second;
+
+        String str= "Hello";
+        String str1= "Hello";
+
+        if ((a = true) | (b = true) | (c = true))
+            System.out.println(a + " " + b + " " + c);
+    }
+}
